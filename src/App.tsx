e3f836f20ba5905cb1087d97be065685241f9e25
Emmanuel Nwachi <egreatness1@gmail.com>
2020-12-11 13:01:02 +0100

    'initial commit - homepage -custom theme with darkmode -social media contact '

--- src/App.tsx
+++ src/App.tsx
@@ -2,55 +2,77 @@ import * as React from "react";
 import {
   ChakraProvider,
   Button,
+  Heading,
   Box,
   Text,
   VStack,
   HStack,
   Grid,
+  Icon,
+  Link,
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
 import { theme } from "./theme";
+import { FaLinkedinIn, FaTwitter, FaGithub } from "react-icons/fa";
+
+//WORK HISTORY GRID:
+// -> one long left border line with arrows pointing to table of contents
+// -> when each item on the T.O.C is clicked, it should reveal the work history content
+// -> when another item is clicked, the previous content fades out, new content shows up
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={6} w="440px" mx="auto">
-          <h1>
-            Hello, I'm <br /> Emmanuel Nwachukwu
-          </h1>
-          <Text color="darkgray">
-            Software developer who is focused on the Frontend stack and lives in
-            Lagos, Nigeria.{" "}
+      <Grid minH="100vh" py={3} px={6} placeItems="center">
+        <ColorModeSwitcher justifySelf="flex-end" alignSelf="flex-start" />
+        <VStack spacing={5} mx="auto">
+          <Heading as="h1" size="2xl" lineHeight="64px">
+            Hello!&#128512; I'm <br /> Emmanuel Nwachukwu
+          </Heading>
+          <Text
+            color="darkgray"
+            maxW="460px"
+            p={4}
+            fontSize={{ base: "18px", md: "22px", lg: "28px" }}
+          >
+            Software developer who is in love with the Frontend stack and lives
+            in Lagos, Nigeria.{" "}
           </Text>
-          <HStack spacing={8}>
-            {/* Button 1, View Resume */}
-            <Button>Where I've Worked?</Button>
+          <HStack spacing={[4, 6, 8, 10]} mb={6}>
+            {/* Button 1, when clicked; 
+            -should split into two grids and 
+            -slide in the Resume/Work history from the left into the page
+            -push the default single grid page to the right side(2nd column) of the new two column grids
+            -there should be an exit button that restores the default single grid
+            */}
+            <Button>Work History</Button>
 
-            {/* Button 2, View Projects */}
+            {/* Button 2, when clicked; 
+            -should split into two grids and 
+            -slide in the content from the right into the page
+            -push the default single grid page to the left side(1st column) of the new two column grids
+            -there should be an exit button that restores the default single grid
+            */}
             <Button>My Projects</Button>
-
-            {/* <Link
-              color="teal.500"
-              outlineColor="none"
-              href="https://github.com/nwachiii"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Github
-            </Link>
+          </HStack>
+          <HStack spacing={[4, 6, 8, 10]}>
             <Link
-              color="teal.500"
-              href="https://twitter.com/nwachiii"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
+              bg="white"
+              rounded={4}
+              px={1}
+              pb={1}
+              href="https://linkedin.com/in/nwachukwuemmauel"
+              isExternal
             >
-              Twitter
-            </Link> */}
+              <Icon as={FaLinkedinIn} w={6} h={6} color="#0077B5" />{" "}
+            </Link>
+            <Link href="https://github.com/nwachiii" isExternal>
+              <Icon as={FaGithub} w={8} h={8} />
+            </Link>
+            <Link href="https://twitter.com/nwachiii" isExternal>
+              <Icon as={FaTwitter} w={8} h={8} color="#1DA1F2" />
+            </Link>
           </HStack>
         </VStack>
       </Grid>
