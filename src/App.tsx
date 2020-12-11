2ef80f275c2dd7d74d0940c50af904694ca62f3a
Emmanuel Nwachi <egreatness1@gmail.com>
2020-12-11 10:44:12 +0100

    customized chakra button component in theme.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
 import * as React from "react";
 import {
   ChakraProvider,
+  Button,
   Box,
   Text,
-  Link,
   VStack,
-  Code,
+  HStack,
   Grid,
-  theme,
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
+
+import { theme } from "./theme";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <h1>Hello Emmanuel Nwachukwu</h1>
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+        <VStack spacing={6} w="440px" mx="auto">
+          <h1>
+            Hello, I'm <br /> Emmanuel Nwachukwu
+          </h1>
+          <Text color="darkgray">
+            Software developer who is focused on the Frontend stack and lives in
+            Lagos, Nigeria.{" "}
           </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+          <HStack spacing={8}>
+            {/* Button 1, View Resume */}
+            <Button>Where I've Worked?</Button>
+
+            {/* Button 2, View Projects */}
+            <Button>My Projects</Button>
+
+            {/* <Link
+              color="teal.500"
+              outlineColor="none"
+              href="https://github.com/nwachiii"
+              fontSize="2xl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </Link>
+            <Link
+              color="teal.500"
+              href="https://twitter.com/nwachiii"
+              fontSize="2xl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </Link> */}
+          </HStack>
         </VStack>
       </Grid>
     </Box>
