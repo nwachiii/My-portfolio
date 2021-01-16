f9c90c86a0d49458e268ae11cc1337552e7d5247
Emmanuel Nwachi <egreatness1@gmail.com>
2021-01-16 22:10:44 +0100

    'Cards Component'

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import * as React from "react";
 import { theme } from "./theme/theme";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, CSSReset, SimpleGrid } from "@chakra-ui/react";
 
-import { Main } from "./Components/Main";
+// import { Main } from "./Components/Main";
 import { Footer } from "./Components/Footer";
+import { Cards } from "./Components/Cards/Cards";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Main />
+    <CSSReset />
+    {/* <Main /> */}
+    <SimpleGrid
+      columns={[1, 1, 2, 2, 3]}
+      spacingX="20px"
+      spacingY="40px"
+      justifyContent="center"
+      p={[4, 6, 8, 10]}
+      mb={8}
+    >
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+    </SimpleGrid>
     <Footer />
   </ChakraProvider>
 );
---
+++ src/Components/Cards/Cards.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import {
+  Box,
+  Heading,
+  HStack,
+  Icon,
+  Link,
+  Text,
+  Tooltip,
+  VStack,
+} from "@chakra-ui/react";
+import { HiOutlineCode, HiOutlineEye, HiThumbUp } from "react-icons/hi";
+import "./CardStyles.css";
+
+export function Cards() {
+  // const { projectTitle, projectDesc, projectTools, cardIcon} = props;
+  return (
+    <>
+      <Box className="card__wrapper" my={[2, 2, 4, 8]}>
+        <Box className="card__box">
+          <VStack className="glass-effect" spacing={[4, 6]}>
+            <Heading as="h2" fontSize={["lg", "xl", "2xl"]}>
+              P R O J E C T
+            </Heading>
+            <Text fontSize={["md", "lg", "xl"]}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.Officiis
+              mpedit laborum eum omnis dolores minus necessitatibus quo libero
+              ius, eligendi rerum.Facere illo cum, voluptate nisi harum iste
+              velit excepturi.
+            </Text>
+            <Text fontSize={["xs", "md", "xl"]}>
+              <strong>T O O L S : </strong> REACT, CHAKRA-UI, NETLIFY
+            </Text>
+            <HStack
+              spacing={["2em", "4em", "6em"]}
+              w="100%"
+              justifyContent="center"
+            >
+              <Tooltip label="View Code" aria-label="A tooltip">
+                <Link>
+                  <Icon as={HiOutlineCode} w={8} h={8} color="#9BDFAA" />
+                </Link>
+              </Tooltip>
+              <Tooltip label="View Live" aria-label="A tooltip">
+                <Link>
+                  <Icon as={HiOutlineEye} w={8} h={8} color="#9BDFAA" />
+                </Link>
+              </Tooltip>
+              <Tooltip label="Like" aria-label="A tooltip">
+                <Link>
+                  <Icon
+                    as={HiThumbUp}
+                    w={8}
+                    h={8}
+                    color="#9BDFAA"
+                    cursor="pointer"
+                  />
+                </Link>
+              </Tooltip>
+            </HStack>
+          </VStack>
+          <div className="big__circle"></div>
+        </Box>
+      </Box>
+    </>
+  );
+}
